a201380f0733cfb0a9bc2fb8dcf773cf33fdf723
mds-web-dev <webdevelop.mds@gmail.com>
2024-08-08 18:16:42 -0700

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-primary'>Vip Machinaries</h1>
+    </>
+  )
+}
+
+export default App
